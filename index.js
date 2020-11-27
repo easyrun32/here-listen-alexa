ba1108894b2bfaf10813caa3156cc3ba398594dc
bryan aviles <easyrun32@gmail.com>
2020-11-27 12:49:49 -0500

    helper function must work?

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Alexa = require("ask-sdk-core");
 const https = require("https");
 //testing from github
 const invocationName = "here listen";
-const LaunchRequest_Handler = require("./Handlers/launch-handler");
+
 function getMemoryAttributes() {
   const memoryAttributes = {
     history: [],
@@ -31,6 +31,8 @@ function getMemoryAttributes() {
 
 const maxHistorySize = 20; // remember only latest 20 intents
 
+// const LaunchRequest_Handler = require("./Handlers/launch-handler");
+const getRemoteData = require("./helpers/getRemoteData");
 // 1. Intent Handlers =============================================
 
 // Helper
@@ -52,60 +54,60 @@ const maxHistorySize = 20; // remember only latest 20 intents
 //When Applications Opens
 //https://developer.amazon.com/blogs/post/Tx3CX1ETRZZ2NPC/Alexa-Account-Linking-5-Steps-to-Seamlessly-Link-Your-Alexa-Skill-with-Login-wit
 
-// const LaunchRequest_Handler = {
-//   canHandle(handlerInput) {
-//     const request = handlerInput.requestEnvelope.request;
-//     // && handlerInput.requestEnvelope.request.intent.name
-//     return request.type === "LaunchRequest";
-//   },
-//   async handle(handlerInput) {
-//     const {
-//       accessToken,
-//       userId,
-//     } = handlerInput.requestEnvelope.context.System.user;
-//     /*
-//     post is user registered?
+const LaunchRequest_Handler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    // && handlerInput.requestEnvelope.request.intent.name
+    return request.type === "LaunchRequest";
+  },
+  async handle(handlerInput) {
+    const {
+      accessToken,
+      userId,
+    } = handlerInput.requestEnvelope.context.System.user;
+    /*
+    post is user registered?
 
-//     if yes then make a post request to see if they exist if they do  dont do anything
-//     if no then make a post request and register them
+    if yes then make a post request to see if they exist if they do  dont do anything
+    if no then make a post request and register them
 
-//     */
-//     // const { userId } = handlerInput.requestEnvelope.context.System.user;
-//     let speechText = "";
+    */
+    // const { userId } = handlerInput.requestEnvelope.context.System.user;
+    let speechText = "";
 
-//     if (!accessToken) {
-//       speechText =
-//         "You must authenticate with your Amazon Account to use this skill. I sent instructions for how to do this in your Alexa App";
-//       return handlerInput.responseBuilder
-//         .speak(speechText)
-//         .withLinkAccountCard()
-//         .getResponse();
-//     } else {
-//       let url = `https://api.amazon.com/user/profile?access_token=${accessToken}`;
-//       await getRemoteData(url)
-//         .then((response) => {
-//           const data = JSON.parse(response);
-//           // invocationName is a variable
-//           speechText = `hi ${data.name}. You are registered with ${data.email}. How can i help your team?`;
-//         })
-//         .catch((err) => {
-//           speechText = err.message;
-//         });
-//       let say = `say open ${invocationName}`;
+    if (!accessToken) {
+      speechText =
+        "You must authenticate with your Amazon Account to use this skill. I sent instructions for how to do this in your Alexa App";
+      return handlerInput.responseBuilder
+        .speak(speechText)
+        .withLinkAccountCard()
+        .getResponse();
+    } else {
+      let url = `https://api.amazon.com/user/profile?access_token=${accessToken}`;
+      await getRemoteData(url)
+        .then((response) => {
+          const data = JSON.parse(response);
+          // invocationName is a variable
+          speechText = `hi ${data.name}. You are registered with ${data.email}. How can i help your team?`;
+        })
+        .catch((err) => {
+          speechText = err.message;
+        });
+      let say = `say open ${invocationName}`;
 
-//       return handlerInput.responseBuilder
-//         .speak(speechText)
-//         .reprompt("try again, " + say)
-//         .withStandardCard(
-//           "Welcome!",
-//           "Hello!\nThis is a card for your skill, " + invocationName,
-//           welcomeCardImg.smallImageUrl,
-//           welcomeCardImg.largeImageUrl
-//         )
-//         .getResponse();
-//     }
-//   },
-// };
+      return handlerInput.responseBuilder
+        .speak(speechText)
+        .reprompt("try again, " + say)
+        .withStandardCard(
+          "Welcome!",
+          "Hello!\nThis is a card for your skill, " + invocationName,
+          welcomeCardImg.smallImageUrl,
+          welcomeCardImg.largeImageUrl
+        )
+        .getResponse();
+    }
+  },
+};
 
 const AMAZON_CancelIntent_Handler = {
   canHandle(handlerInput) {
